8c4f87f4ae7655677971ce88d409ef38f0a9be3b
Ayantika Paul <ayantikapaul137@gmail.com>
2025-03-21 12:06:26 +0530

    Except Auto scrolling and moving round rest are working

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,12 +1,7 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
 
-ChatForm.propTypes = {
-  setChatHistory: PropTypes.func.isRequired,
-};
-
-
-const ChatForm = ({setChatHistory}) => {
+const ChatForm = ({chatHistory, setChatHistory, generateBotResponse}) => {
     const inputRef = useRef();
 
     const handleFormSubmit = (e) => {
@@ -16,7 +11,17 @@ const ChatForm = ({setChatHistory}) => {
         inputRef.current.value = "";
 
         //update chat history with the user's message
-        setChatHistory((history) => [...history, { role: "user", text: userMessage }])
+        setChatHistory((history) => [...history, { role: "user", text: userMessage }]);
+
+        //Delay 600 ms before showing "Thinking..." and generating response
+        setTimeout(() => {
+
+          //Add a "Thinking..." placeholder for the bot's response 
+          setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]);
+
+          //Call the function to generate Bot's response
+          generateBotResponse([...chatHistory, {role: "user", text: `Using the details provided above, please address this query: ${userMessage}` }]);
+        },600);
     };
   return (
     <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
